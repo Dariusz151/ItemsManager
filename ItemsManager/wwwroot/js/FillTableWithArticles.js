2b3092425349e5196bc66c9a34b69113139a1f46
Dariusz <dariusz151@gmail.com>
2018-10-14 22:49:07 +0200

    dko/css and boostrap editing

--- ItemsManager/wwwroot/js/FillTableWithArticles.js
+++ ItemsManager/wwwroot/js/FillTableWithArticles.js
@@ -6,7 +6,7 @@
 
     $("#cont_tableBody").children().remove();
     $("#cont_tableBody").append(" <table class='table table-hover'><tbody id='myTable'></tbody></table> ");
-
+   
     $.each(articlesTable, function (index, item) {
         AddFridgeItemToTable(index, item);
     });
@@ -16,19 +16,19 @@ function AddFridgeItemToTable(index, item) {
    
     $("#myTable").append("<tr class='row' id='row" + index + "'></tr>")
 
-    $("#row" + index).append("<td class='col col_articleName'></td");
+    $("#row" + index).append("<td class='col-7 col_articleName'></td");
     $("#row" + index + " .col_articleName").html(item.articleName);
 
-    $("#row" + index).append("<td class='col col_quantity'></td");
+    $("#row" + index).append("<td class='col-2 text-center col_quantity'></td");
     $("#row" + index + " .col_quantity").html(item.quantity);
 
-    $("#row" + index).append("<td class='col col_weight'></td");
+    $("#row" + index).append("<td class='col-2 text-center col_weight'></td");
     $("#row" + index + " .col_weight").html(item.weight);
 
     //$("#row" + index).append("<td class='col-lg-1 col_functions'></td>");
     //$("#row" + index + " .col_functions").html("<input class='form-check-input position-static' type='checkbox' id='checkbox" + index + "' value='option1' aria-label='aria'>");
 
-    $("#row" + index).append("<td class='col-lg-1 col_functions'></td>");
+    $("#row" + index).append("<td class='col-lg-1 text-center col_functions'></td>");
     $("#row" + index + " .col_functions").html("<label class='customcheck'><input type='checkbox' autocomplete='off'><span class='checkmark'></span></label>");
 
 
