b1c00c3a69585271cb44dc9bb06fca06ae84ce10
Dariusz <dariusz151@gmail.com>
2019-02-26 20:16:13 +0100

    added SmartFridgeExceptions

---
+++ ItemsManager/Common/Exceptions/SmartFridgeException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ItemsManager.Common.Exceptions
+{
+    public class SmartFridgeException : Exception
+    {
+        public string Code { get; }
+
+        public SmartFridgeException()
+        {
+        }
+
+        public SmartFridgeException(string code)
+        {
+            Code = code;
+        }
+
+        public SmartFridgeException(string message, params object[] args) : this(string.Empty, message, args)
+        {
+        }
+
+        public SmartFridgeException(string code, string message, params object[] args) : this(null, code, message, args)
+        {
+        }
+
+        public SmartFridgeException(Exception innerException, string message, params object[] args)
+            :this (innerException, string.Empty, message, args)
+        {
+        }
+
+        public SmartFridgeException(Exception innerException, string code, string message, params object[] args)
+            : base (string.Format(message, args), innerException)
+        {
+            Code = code;
+        }
+    }
+}
